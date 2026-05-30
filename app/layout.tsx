102d9c6f04850c36f45475f445b870719f7267ae
Bailey Luu <bailey.luu30@gmail.com>
2026-05-30 00:08:34 -0400

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { SandpackCSS } from './blog/[slug]/sandpack';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://bailey-nextjs-portfolio.vercel.app'),
@@ -21,26 +20,7 @@ export const metadata: Metadata = {
     siteName: 'Bailey Luu',
     locale: 'en_US',
     type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    title: 'Lee Robinson',
-    card: 'summary_large_image',
-  },
-  verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
-  },
+  }
 };
 
 const cx = (...classes) => classes.filter(Boolean).join(' ');
@@ -60,7 +40,6 @@ export default function RootLayout({
       )}
     >
       <head>
-        <SandpackCSS />
       </head>
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
